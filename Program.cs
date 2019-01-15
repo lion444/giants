0f7cf79c78f185fd80b4d810efa0c42daf071fb4
mike <none>
2019-01-15 12:44:25 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace giants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.eat("Bananas");
+        }
+
+        public void eat(String food){
+            Console.WriteLine($"Im eating {food}");
+        }
+    }
+}
